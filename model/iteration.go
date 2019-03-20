45b79e9e4758e5d5febc3d0280e7a5d9bce9410a
Wei-Chun <chu@fitanalytics.com>
2019-03-20 18:39:17 +0100

    build flipping and moving paths

--- model/iteration.go
+++ model/iteration.go
@@ -18,15 +18,33 @@ func Iterate(s State, t float64) State {
 	siteConns := s.connections[siteID]
 	siteSpin := s.spins[siteID]
 
-	// get distances from operational site to each site (including itself)
-	distances := make([]float64, numSites)
-	for id, loc := range s.locations {
-		distances[id] = math.Sqrt(math.Pow(siteLoc.x-loc.x, 2) + math.Pow(siteLoc.y-loc.y, 2))
-	}
-	neighborIDs := GetNeighbors(distances, siteID, siteConns)
+	// get neighbors from current operational site
+	currNeighbors := GetNeighbors(siteID, siteLoc, s.locations, siteConns)
+	currE := GetEnergy(siteSpin, currNeighbors, s.spins)
+	fmt.Printf("current energy = %f", currE)
 
-	currentE := GetEnergy(siteSpin, neighborIDs, s.spins)
-	fmt.Printf("current energy = %f", currentE)
+	if rand.Float64() < 0.5 { // try flipping spin
+		// in simplest case, flippedE = -currentE, but we calculate it using GetEnergy for completeness
+		flippedE := GetEnergy(-siteSpin, currNeighbors, s.spins)
+		fmt.Printf("flipped energy = %f", flippedE)
+
+		excitation := flippedE - currE
+		// if flipping drops energy, flip it
+		// if flipping raises energy, use condition
+		if excitation < 0 || rand.Float64() < math.Exp(-excitation/t) {
+			s.spins[siteID] = -siteSpin
+			return s
+		}
+
+	} else {
+		// if moved
+		candLoc := Location{x: rand.Float64(), y: rand.Float64()}
+		fmt.Printf("proposed new location = %+v", candLoc)
+		candNeighbors := GetNeighbors(siteID, candLoc, s.locations, siteConns)
+		candE := GetEnergy(siteSpin, candNeighbors, s.spins)
+		fmt.Printf("energy if moved to proposed location = %f", candE)
+
+	}
 
 	return s
 }
@@ -35,11 +53,17 @@ func Iterate(s State, t float64) State {
 // ds = array of distances from the site ordered by site ids
 // id0 = the id of the operational site
 // nc = num of connections
-func GetNeighbors(ds []float64, id0 int, nc int) []int {
+func GetNeighbors(id0 int, loc0 Location, locs []Location, nc int) []int {
+	// calculate distances from all operational site to all sites
+	distances := make([]float64, len(locs))
+	for id, loc := range locs {
+		distances[id] = math.Sqrt(math.Pow(loc0.x-loc.x, 2) + math.Pow(loc0.y-loc.y, 2))
+	}
+
 	// convert indices of ds to an array
-	ids := make([]int, len(ds))
-	vals := make([]float64, len(ds))
-	for i, d := range ds {
+	ids := make([]int, len(distances))
+	vals := make([]float64, len(distances))
+	for i, d := range distances {
 		ids[i] = i
 		vals[i] = d
 	}
@@ -50,7 +74,7 @@ func GetNeighbors(ds []float64, id0 int, nc int) []int {
 	// collect neighbors
 	var neighbors []int
 	for _, id := range ids {
-		// skip the operational site itself
+		// skip the operational site as its own neighbor
 		if id != id0 {
 			neighbors = append(neighbors, id)
 		}
