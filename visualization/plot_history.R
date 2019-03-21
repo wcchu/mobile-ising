f65fd869c357bf42ee95b8e2321be7560ac3e647
Wei-Chun Chu <weichun.chu@gmail.com>
2019-03-21 23:26:02 +0100

    plot state hist and mag hist

--- visualization/plot_history.R
+++ visualization/plot_history.R
@@ -1,9 +1,14 @@
-suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(tidyverse))
 
+## state history
 state_hist <- read.csv('../model/state_hist.csv')
+state_plot <-
+  ggplot(state_hist %>% filter(time == 0.0)) +
+  geom_point(aes(x = x, y = y, color = spin))
+print(state_plot)
 
+## magnetization history
 mag_hist <- read.csv('../model/mag_hist.csv')
-
 mag_plot <-
   ggplot(mag_hist) +
   geom_point(aes(x = time, y = mag))
