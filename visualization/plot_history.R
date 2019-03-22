98d211337ac33f1a80ed017eab03eb86ac6695cd
Wei-Chun <chu@fitanalytics.com>
2019-03-22 14:41:08 +0100

    visualize scan

--- visualization/plot_history.R
+++ visualization/plot_history.R
@@ -5,12 +5,13 @@ state_hist <- read.csv('../model/state_hist.csv')
 state_plot <-
   ggplot(state_hist) +
   geom_point(aes(x = x, y = y, color = spin)) +
-  facet_grid(time ~ .)
+  facet_grid(time ~ temp)
 print(state_plot)
 
 ## magnetization history
 mag_hist <- read.csv('../model/mag_hist.csv')
 mag_plot <-
   ggplot(mag_hist) +
-  geom_point(aes(x = time, y = mag))
+  geom_point(aes(x = time, y = mag)) +
+  facet_grid(. ~ temp)
 print(mag_plot)
